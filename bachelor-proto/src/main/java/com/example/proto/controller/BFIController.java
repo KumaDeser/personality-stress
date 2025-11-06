8dcf90219a1fa01b06f834b14b8490e441706cfb
eser <eser.guersu@gmail.com>
2025-11-06 19:49:48 +0100

    Initial commit

---
+++ bachelor-proto/src/main/java/com/example/proto/controller/BFIController.java
@@ -0,0 +1,12 @@
+package com.example.proto.controller;
+
+import java.util.List;
+
+public class BFIController {
+
+    List<String> personalityTypes;
+
+    public BFIController() {
+        personalityTypes = new List<String>(2);
+    }
+}
---
+++ bachelor-proto/src/main/java/com/example/proto/controller/StressLevelController.java
@@ -0,0 +1,11 @@
+package com.example.proto.controller;
+
+import java.util.List;
+
+public class StressLevelController {
+
+    // Liste mit Stressleveln (integer/strings)
+    List<Strings> stressLevels = [];
+
+    private static calculateStressLevel()
+}
