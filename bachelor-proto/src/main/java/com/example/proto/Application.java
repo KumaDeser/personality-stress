ec20c007eda78e4898e4dd4d37609c6c783b5863
eser <eser.guersu@gmail.com>
2025-12-10 01:59:24 +0100

    chore: add comment to trigger redeploy in Application.java

--- bachelor-proto/src/main/java/com/example/proto/Application.java
+++ bachelor-proto/src/main/java/com/example/proto/Application.java
@@ -2,7 +2,7 @@ package com.example.proto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Trigger redeploy
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
